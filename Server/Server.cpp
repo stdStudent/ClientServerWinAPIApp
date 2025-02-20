09f8f26ef26a114e42a5af0742698c6d7385e07c
stdStudent <78802930+stdStudent@users.noreply.github.com>
2025-02-20 17:11:21 +0500

    [ADD]: support multiple users.

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_NON_CONFORMING_SWPRINTFS
 
 #include <winsock2.h>
 #include <windows.h>
@@ -7,6 +8,7 @@
 #include <stdlib.h>
 #include <direct.h>
 #include <string>
+#include <vector>
 #include <shlwapi.h>
 
 #pragma comment(lib, "ws2_32.lib")
@@ -17,10 +19,14 @@
 #define BUFFER_SIZE 4096
 
 typedef struct {
+	char login[32];
+	char password[32];
+} User;
+
+typedef struct {
     int port;
     char directory[MAX_PATH];
-    char login[32];
-    char password[32];
+	std::vector<User> users;
 } ServerConfig;
 
 std::wstring pwd() {
@@ -33,8 +39,6 @@ std::wstring pwd() {
 void LoadConfig(ServerConfig* config) {
     wchar_t port[32];
     wchar_t directory[MAX_PATH];
-    wchar_t login[32];
-    wchar_t password[32];
 
     auto path = pwd();
     std::wstring pathToIni = path + L"\\" + CONFIG_FILE;
@@ -46,25 +50,44 @@ void LoadConfig(ServerConfig* config) {
 
     GetPrivateProfileStringW(L"Server", L"Port", L"8080", port, 32, pathToIni.c_str());
     GetPrivateProfileStringW(L"Server", L"Directory", L".\\server_files", directory, MAX_PATH, pathToIni.c_str());
-    GetPrivateProfileStringW(L"Auth", L"Login", L"windows", login, 32, pathToIni.c_str());
-    GetPrivateProfileStringW(L"Auth", L"Password", L"windows", password, 32, pathToIni.c_str());
+
+    wchar_t sectionName[32] = { 0 };
+    for (int i = 1; ;++i) {
+		swprintf(sectionName, L"User%d", i);
+
+        wchar_t log[32] = { 0 };
+        wchar_t pass[32] = { 0 };
+		GetPrivateProfileStringW(sectionName, L"Login", L"", log, 32, pathToIni.c_str());
+		GetPrivateProfileStringW(sectionName, L"Password", L"", pass, 32, pathToIni.c_str());
+
+		if (wcslen(log) == 0 || wcslen(pass) == 0) {
+			break;
+		}
+
+		char login[32] = { 0 };
+		char password[32] = { 0 };
+        wcstombs(login, log, 32);
+        wcstombs(password, pass, 32);
+
+        auto user = User{};
+        strncpy(user.login, login, sizeof(user.login) - 1);
+        strncpy(user.password, password, sizeof(user.password) - 1);
+        user.login[sizeof(user.login) - 1] = '\0';
+        user.password[sizeof(user.password) - 1] = '\0';
+
+        config->users.push_back(user);
+    }
 
     config->port = _wtoi(port);
     wcstombs(config->directory, directory, MAX_PATH);
-    wcstombs(config->login, login, 32);
-    wcstombs(config->password, password, 32);
 }
 
-BOOL Authenticate(char* user, char* pass, ServerConfig* config) {
-    if (strcmp(config->login, "windows") == 0) {
-        HANDLE hToken;
-        if (LogonUserA(user, ".", pass, LOGON32_LOGON_NETWORK, LOGON32_PROVIDER_DEFAULT, &hToken)) {
-            CloseHandle(hToken);
-            return TRUE;
-        }
-        return FALSE;
-    }
-    return strcmp(user, config->login) == 0 && strcmp(pass, config->password) == 0;
+bool Authenticate(char* user, char* pass, ServerConfig* config) {
+	for (auto& u : config->users) {
+		if (strcmp(u.login, user) == 0 && strcmp(u.password, pass) == 0) {
+			return true;
+		}
+	}
 }
 
 DWORD WINAPI ClientHandler(LPVOID lpParam) {
